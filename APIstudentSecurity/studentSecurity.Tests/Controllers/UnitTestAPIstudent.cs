f4a3ebbf7d82c390f12fde8d13b1852d1dffdeff
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2020-09-19 02:40:35 -0400

    Se a creado el UnitTest del ApiWebStudents

---
+++ APIstudentSecurity/studentSecurity.Tests/Controllers/UnitTestAPIstudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using APIstudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace studentSecurity.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTestAPIstudent
+    {
+        [TestMethod]
+        public void TestGetStudent()
+        {
+            //Arrange           objeto            instancia        clase
+            StudentsController studentsController = new StudentsController(); //objeto
+            //Act  nombredelista    objeto           Tabla de varios estudiantes
+            var listaEstudiantes = studentsController.GetStudents();
+            //Assert            nombredelista
+            Assert.IsNotNull(listaEstudiantes);
+        }
+    }
+}
